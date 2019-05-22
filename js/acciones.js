0d834a0a7bde3bcef70f0f887e292aa712697134
app8 <sarapantle23@gmail.com>
2019-05-22 08:25:44 -0500

    10

--- js/acciones.js
+++ js/acciones.js
@@ -1,31 +1,30 @@
-//JavaScript Document
+// JavaScript Document
 
-$(document).ready(function(e) {
-document.addEventListener("deviceready", onDeviceReady, false);
-
+$(document).ready(function(e){
+	document.addEventListener("deviceready",onDeviceReady,false);
 });
 
-function onDeviceReady(){
+function onDeviceReady() {
 	
-$('#posicion').on('click',function(){
-    getPosition ();
-    });
+	$('#posicion').on('click',function() {
+		getPosition();
+	});
 }
 
 function getPosition(){
 	var options={
 		enableHighAccuracy :true,
-		maximumAge: 3600000
+		maximumAge:3600000
 	}
 	
-	var watchID=navigator.geolocation.getCurrentPosition(onSuccess, onError,options);
-	function onSuccess(position){
-		
-		alert('Latitude:'   +position.coords.latitude    +'\n'+
-		   'Longitude:'     +position.coords.longitude   +'\n');
-	);
+	var watchID=navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
 	
-	function onError(error){
-		alert('code:'  +error.code  +'\n'+'message:' + error.message + '\n');
+	function onSuccess(position) {
+		
+		alert('Latitude:'    +position.coords.latitude   +'\n'+
+		'Longitude:'     +position.coords.longitude     +'\n');
+	};
+	function onError(error) {
+		alert('code:'  + error.code   + '\n'  + 'message:' + error.message + '\n');
 	}
 }
